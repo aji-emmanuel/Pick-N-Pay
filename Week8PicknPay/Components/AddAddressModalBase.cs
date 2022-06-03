ccb57f7392d5b8a7f535ce21b37dc21af1dad590
Emmanuel Ajibode <ajiemilo@gmail.com>
2022-06-03 03:26:52 +0100

    Add address form validation

---
+++ Week8PicknPay/Components/AddAddressModalBase.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Week8PicknPay.Models;
+using Week8PicknPay.Repository;
+
+namespace Week8PicknPay.Components
+{
+    public class AddAddressModalBase : ComponentBase
+    {
+        [Inject] IAddressRepository _addressRepo { get; set; }
+
+        protected CustomFormValidator customFormValidator;
+
+        [Parameter]
+        public Order Order { get; set; }
+
+        protected bool show = false;
+
+        protected Address AddressModel = new Address();
+
+        protected async Task HandleSubmit()
+        {
+            show = await _addressRepo.AddAddressAsync(AddressModel);
+            if (show)
+            {
+                Order.Address = AddressModel;
+            }
+        }
+    }
+}
